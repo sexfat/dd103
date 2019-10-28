e2d12f595d7a5c4a49cede31ca42e889da135bc1
sexfat <bryant6831@gmail.com>
2019-10-28 15:33:58 +0800

    add scrollmagic

--- dest/js/animation.js
+++ dest/js/animation.js
@@ -1,4 +1,6 @@
 console.log('start');
+var controller = new ScrollMagic.Controller();
+
 
 
 
@@ -22,25 +24,25 @@ TweenMax.to('.a2', 5, {
 });
 
 
-TweenMax.fromTo('.a3', 4,{
+TweenMax.fromTo('.a3', 4, {
     x: 200
 }, {
     x: 600
 })
 
-TweenMax.staggerFromTo('.a4' , 1 ,{
+TweenMax.staggerFromTo('.a4', 1, {
     x: 300
-   
-},{
-    ease: Elastic.easeOut.config(1, 0.3),
-    x:600
-},0.5);
 
-TweenMax.to('.a5' , 1 , {
-  x:200,
-  repeat: -1,
-  repeatDelay: .6, 
-  yoyo: true
+}, {
+    ease: Elastic.easeOut.config(1, 0.3),
+    x: 600
+}, 0.5);
+
+TweenMax.to('.a5', 1, {
+    x: 200,
+    repeat: -1,
+    repeatDelay: .6,
+    yoyo: true
 });
 // TweenMax.from('.a5' , 1 , {
 //     x:100
@@ -69,19 +71,18 @@ TweenMax.to('.a5' , 1 , {
 //    rotation: 180,
 //    transformOrigin : 'bottom top'
 // })
-TweenMax.fromTo('.b2',1,{
-   x: 100,
-   boxShadow: "0 0 3px rgba(0, 0, 0, 0.2)" //陰影
- },{
-   X: 200
-    
- })
 
 
+//第三個場景
+
+var title03 = TweenMax.to('.c1', 1, {
+    y: 100
+})
 
 
-
-
-
-
-
+var scene = new ScrollMagic.Scene({
+    triggerElement: '#tripper_01',
+     triggerHook: 0,
+     duration: 400,
+     offset: 100
+}).setTween(title03).addIndicators().addTo(controller);
