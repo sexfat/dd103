885dceb650ee3245e0e5c0726ff77012fcb49b41
sexfat <bryant6831@gmail.com>
2019-10-28 11:58:56 +0800

    add

--- dev/js/animation.js
+++ dev/js/animation.js
@@ -1,5 +1,22 @@
 console.log('start');
 
-TweenMax.to('.a1' ,1,{
-    x:100
+
+
+function alerts() {
+    TweenMax.to('.a1' ,1,{
+        x:100
+    });
+}
+
+
+
+
+TweenMax.to('.a2' , 5 ,{
+  x: 200,
+  width: 50,
+//   repeat: -1,
+  rotation: 360,
+//   opacity: 0,
+  ease: Elastic.easeOut.config(1, 0.3),
+  onComplete: alerts
 })
